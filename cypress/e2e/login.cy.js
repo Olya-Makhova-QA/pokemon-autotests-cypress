b364dc643f35b96321a8de244d1472b266afdc2e
Denis <pankratpmr@gmail.com>
2025-12-11 19:59:59 +0300

    Add Cypress autotests for Pokemon Battle

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,115 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/'); //Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверили цвет кнопки вост. пароль
+        
+        cy.get('#mail').type('german@dolnikov.ru'); //Ввели верный логин
+        cy.get('#pass').type('qa_one_love1'); //Ввели верный пароль
+        cy.get('#loginButton').click(); //Нажали кнопку Войти
+
+        cy.get('#messageHeader').contains('Авторизация прошла успешно'); //Проверили, что после автор. виден текст
+        cy.get('#messageHeader').should('be.visible'); //Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Есть крестик и он виден пользователю
+        
+    })
+
+    it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio/'); //Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверили цвет кнопки вост. пароль
+        
+        cy.get('#mail').type('german@dolnikov.ru'); //Ввели верный логин
+        cy.get('#pass').type('qa_one_love7'); //Ввели неверный пароль
+        cy.get('#loginButton').click(); //Нажали кнопку Войти
+
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); //Проверили, что после автор. виден текст
+        cy.get('#messageHeader').should('be.visible'); //Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Есть крестик и он виден пользователю
+        
+    })
+
+    it('Проверка валидации логина', function () {
+        cy.visit('https://login.qa.studio/'); //Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверили цвет кнопки вост. пароль
+        
+        cy.get('#mail').type('germandolnikov.ru'); //Ввели логин без @
+        cy.get('#pass').type('qa_one_love1'); //Ввели верный пароль
+        cy.get('#loginButton').click(); //Нажали кнопку Войти
+
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); //Проверили, виден текст
+        cy.get('#messageHeader').should('be.visible'); //Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Есть крестик и он виден пользователю
+        
+    })
+
+    it('Проверка восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/'); //Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверили цвет кнопки вост. пароль
+        
+        cy.get('#forgotEmailButton').click(); //Нажали кнопку Забыли пароль
+
+        cy.get('#mailForgot').type('german@dolnikov.ru'); //Ввели почту для восстановления пароля
+        cy.get('#restoreEmailButton').click(); //Нажали Отправить код
+
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); //Текст совпадает
+        cy.get('#messageHeader').should('be.visible'); //Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Есть крестик и он виден пользователю
+        
+    })
+
+    it('Неверный логин и верный пароль', function () {
+    cy.visit('https://login.qa.studio/'); //Зашли на сайт
+    cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); //Проверили цвет кнопки вост. пароль
+
+    cy.get('#mail').type('wrong@dolnikov.ru'); //Ввели неверный логин
+    cy.get('#pass').type('qa_one_love1'); //Ввели верный пароль
+    cy.get('#loginButton').click(); //Нажали кнопку Войти
+
+    cy.get('#messageHeader').contains('Такого логина или пароля нет'); //Проверили нужный текст
+    cy.get('#messageHeader').should('be.visible'); //Текст виден пользователю
+    cy.get('#exitMessageButton > .exitIcon').should('be.visible'); //Есть крестик
+
+    });
+
+    it('Проверка приведения логина к строчным буквам', function () {
+    cy.visit('https://login.qa.studio/'); // Зашли на сайт
+    cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверили цвет кнопки
+
+    cy.get('#mail').type('GerMan@Dolnikov.ru'); // Логин с разным регистром
+    cy.get('#pass').type('qa_one_love1'); // Верный пароль
+    cy.get('#loginButton').click(); // Нажали Войти
+
+    cy.get('#messageHeader').contains('Авторизация прошла успешно'); // Тест должен упасть — и это нормально
+    cy.get('#messageHeader').should('be.visible'); 
+    cy.get('#exitMessageButton > .exitIcon').should('be.visible'); 
+
+    });
+
+
+
+    describe('Проверка покупки нового аватара', function () {                 // название набора тестов
+    it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
+         cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
+         cy.get('input[id="k_email"]').type('USER_LOGIN');                   // вводим логин
+         cy.get('input[id="k_password"]').type('USER_PASSWORD');               // вводим пароль
+         cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
+         cy.wait(2000);
+         cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
+         cy.wait(2000);
+         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
+         cy.get('.available > button').first().click();   // кликаем Купить у первого доступного аватара
+         cy.get('.card_number').type('4620869113632996');                     // вводим номер карты
+         cy.get('.card_csv').type('125');                             // вводим CVV карты
+         cy.get('.card_date').type('1226');                           // вводим срок действия карты
+         cy.get('.card_name').type('NAME');                           // вводим имя владельца действия карты
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();     // нажимаем кнопку Оплатить
+         cy.get('.threeds_number').type('56456');                            // вводим код подтверждения СМС
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();   // нажимаем кнопку Оплатить
+         cy.contains('Покупка прошла успешно').should('be.visible');     // проверяем наличие и видимость сообщения об успешной покупке
+     });
+ });
+
+
+
+ })
+
